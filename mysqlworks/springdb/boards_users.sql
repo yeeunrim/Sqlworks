e9b55d0782203980eb057317cce26df75d4e1631
yeeunrim <1515eun@daum.net>
2024-01-07 16:18:15 +0900

    upload20240107

--- mysqlworks/springdb/boards_users.sql
+++ mysqlworks/springdb/boards_users.sql
@@ -13,7 +13,7 @@ create table users(
     userage     int 
 );
 
--- boards 테이블
+-- users를 참조하는 boards 테이블 생성
 create table boards(
 	id           bigint primary key auto_increment,
     boardtitle   varchar(100) not null,
@@ -25,8 +25,31 @@ create table boards(
     foreign key(userid) references users(userid) on delete cascade
 );
 
+-- boards를 참조하는 reply 테이블 생성
+desc boards; -- type 확인
+create table reply(
+	id 			  bigint primary key auto_increment,
+    boardid 	  bigint,
+    replyer 	  varchar(20) not null,
+	replycontent  varchar(1000) not null,
+    createdtime   datetime default now(),
+    updatedtime   datetime,
+    foreign key(boardid) references boards(id) on delete cascade
+);
+
+-- select
 select * from users;
 select * from boards;
+select * from reply;
 
+-- insert (재귀복사)
+insert into boards (boardtitle, userid, boardcontent)
+(select boardtitle, userid, boardcontent from boards);
+
+-- count (전체)
+select count(id) from boards;
+
+-- drop
 drop table if exists users;
 drop table if exists boards;
+drop table if exists reply;
