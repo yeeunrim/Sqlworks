61d87a2384c1ca0236518a13093b6a03fa8aec6a
yeeunrim <1515eun@daum.net>
2024-01-09 18:14:18 +0900

    upload20240108

--- mysqlworks/sysdb/tbl_todo.sql
+++ mysqlworks/sysdb/tbl_todo.sql
@@ -9,11 +9,31 @@ create table tbl_todo(
     createddate  datetime default now()
 );
 
+-- insert
+insert into tbl_todo(title, writer) 
+values ('빠르게 집에 가기', '임예은');
+insert into tbl_todo(title, writer) 
+values ('배운 내용 복습하기', '임예은');
+insert into tbl_todo(title, writer) 
+values ('신년 계획 세우기', '임예은');
+insert into tbl_todo(title, writer) 
+values ('일주일에 3회 운동하기', '임예은');
+insert into tbl_todo(title, writer) 
+values ('12시 전에 잠들기', '임예은');
+
+-- insert (재귀복사)
+insert into tbl_todo(title, writer)
+(select title, writer from tbl_todo);
+
 -- select
-select * from tbl_todo;
+select * from tbl_todo order by tno desc;
+select * from tbl_todo order by tno asc limit 3;
+select * from tbl_todo order by tno asc limit 2, 4;
+select count(tno) from tbl_todo;
+-- 검색 
+select * from tbl_todo
+where title like '%집%' or writer like '%예은%'
+order by tno desc;
 
 -- drop
 drop table if exists tbl_todo;
-
-
-
