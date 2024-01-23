81c634d814dea46bfff9f3d1cef818699f4ea848
yeeunrim <1515eun@daum.net>
2024-01-23 09:38:54 +0900

    upload20240122

--- mysqlworks/bootdb/bootdb.sql
+++ mysqlworks/bootdb/bootdb.sql
@@ -4,8 +4,24 @@ create database bootdb;
 -- db 사용
 use bootdb;
 
--- board 구조
+-- 테이블 구조
 desc board;
+desc member;
+desc t_board;
+desc t_member;
+desc tbl_member;
+desc tbl_board;
 
--- select
+-- 조회
 select * from board;
+select * from member;
+select * from board order by id asc;
+select * from board order by id desc;
+
+-- bootboard
+select * from tbl_member;
+select * from tbl_board order by id desc;
+
+-- bootboard2
+select * from t_board;
+select * from t_member;
